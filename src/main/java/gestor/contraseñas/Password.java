aff6fd1ba30d40b2b591154a5ee0bcf3b07eb307
Gean <GEAN10cuenta@gmail.com>
2025-04-14 15:41:14 -0500

    Agregué metodos a la clase Password

--- src/main/java/gestor/contraseñas/Password.java
+++ src/main/java/gestor/contraseñas/Password.java
@@ -1,4 +1,34 @@
 package gestor.contraseñas;
 public class Password {
-    
+     private String sitioWeb;
+    private String contraseña;
+
+    // Constructor
+    public Password(String sitioWeb, String contraseña) {
+        this.sitioWeb = sitioWeb;
+        this.contraseña = contraseña;
+    }
+
+    // Métodos Getter y Setter
+    public String getSitioWeb() {
+        return sitioWeb;
+    }
+
+    public void setSitioWeb(String sitioWeb) {
+        this.sitioWeb = sitioWeb;
+    }
+
+    public String getContraseña() {
+        return contraseña;
+    }
+
+    public void setContraseña(String contraseña) {
+        this.contraseña = contraseña;
+    }
+
+    // Método para mostrar la información de la contraseña
+    @Override
+    public String toString() {
+        return "Sitio Web: " + sitioWeb + "\nContraseña: " + contraseña;
+    }
 }
